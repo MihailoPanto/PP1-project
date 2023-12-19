63f96a323f2620c8a7ef7821f1cb986dc3b7132d
pm200120d <pm.mihailo@gmail.com>
2023-12-19 23:10:07 +0100

    Add initial code for lexer and base for parser

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/ast/Visitor.java
@@ -0,0 +1,12 @@
+// generated with ast extension for cup
+// version 0.8
+// 19/11/2023 20:26:17
+
+
+package rs.ac.bg.etf.pp1.ast;
+
+public interface Visitor { 
+
+    public void visit(Program Program);
+
+}
---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/ast/VisitorAdaptor.java
@@ -0,0 +1,14 @@
+// generated with ast extension for cup
+// version 0.8
+// 19/11/2023 20:26:17
+
+
+package rs.ac.bg.etf.pp1.ast;
+
+public abstract class VisitorAdaptor implements Visitor { 
+
+    public void visit(Program Program) { visit(); }
+
+
+    public void visit() { }
+}
